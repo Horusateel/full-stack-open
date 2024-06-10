a998aeca1790823c3d641c8c00b712babab15de7
Horusateel <moha3zt@yahoo.com>
2024-06-10 03:29:34 +0200

    ready to continue, installed nodemon

--- Part-1/index.js
+++ Part-1/index.js
@@ -1,4 +1,34 @@
-const http = require('http')
+// const http = require('http')
+
+// let notes = [
+//     {
+//       id: 1,
+//       content: "HTML is easy",
+//       important: true
+//     },
+//     {
+//       id: 2,
+//       content: "Browser can execute only JavaScript",
+//       important: false
+//     },
+//     {
+//       id: 3,
+//       content: "GET and POST are the most important methods of HTTP protocol",
+//       important: true
+//     }
+//   ]
+// const app = http.createServer((req, res) => {
+//     res.writeHead(200, {'Content-Type': 'application/json'})
+//     res.end(JSON.stringify(notes))
+// })
+
+// const PORT = 3001
+// app.listen(PORT)
+// console.log(`Server running on port ${PORT}`)
+
+
+const express = require('express')
+const app = express()
 
 let notes = [
     {
@@ -17,11 +47,19 @@ let notes = [
       important: true
     }
   ]
-const app = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'application/json'})
-    res.end(JSON.stringify(notes))
-})
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+  app.get('/', (req, res) => {
+    res.send('<h1>Hello world!</h1>')
+  })
+
+  app.get('/api/notes/:id', (req, res) => {
+    const id = req.params.id
+    console.log(id)
+    res.json(notes[id])
+    console.log(notes[id])
+  })
+
+  const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+  })
