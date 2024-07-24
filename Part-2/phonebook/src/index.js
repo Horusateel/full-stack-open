9c73051e2d3d3c5b044b68ea332215856da0b44b
Horusateel <moha3zt@yahoo.com>
2024-07-24 20:19:28 +0200

    deleted unrelated dummy data

--- Part-2/phonebook/src/index.js
+++ Part-2/phonebook/src/index.js
@@ -5,27 +5,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true
-  }
-]
-
 root.render(
   <React.StrictMode>
-    <App notes={notes}/>
+    <App />
   </React.StrictMode>
 );
 
