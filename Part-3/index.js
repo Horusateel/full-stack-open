e57df1e9f761e07cd86b985d573d751d5f08e58b
Horusateel <moha3zt@yahoo.com>
2024-08-13 23:19:46 +0200

    a little restructure, .gitignore added, exercises 3.7-3.8

--- Part-3/index.js
+++ Part-3/index.js
@@ -24,8 +24,11 @@ let persons = [
 
 const express = require('express')
 const app = express()
-
 app.use(express.json())
+
+const morgan = require('morgan')
+morgan.token('body', (req) => req.method === 'POST' ? JSON.stringify(req.body) : null)
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/api/persons', (req, res) => {
   res.status(200).json(persons)
