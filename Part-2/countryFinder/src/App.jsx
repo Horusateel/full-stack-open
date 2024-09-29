48acb5b4d02064a8654be2484c7a0ba08388c6f2
Horusateel <moha3zt@yahoo.com>
2024-09-30 01:35:08 +0300

    2.20

--- Part-2/countryFinder/src/App.jsx
+++ Part-2/countryFinder/src/App.jsx
@@ -2,9 +2,11 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const App = () => {
+  // initializing variables
   const [country, setCountry] = useState('')
   const [countries, setCountries] = useState([])
   const [filtered, setFiltered] = useState([])
+  const [weather, setWeather] = useState({temperature: '', wind: ''})
 
   // initialize the data
   useEffect(() => {
@@ -22,7 +24,11 @@ const App = () => {
   // different displays of results: one outcome - < 10 outcomes - > 10 outcomes
   if (filtered.length == 1) {
     const lang = Object.values(filtered[0].languages).map(ele => <li key={ele}>{ele}</li>)
-
+    const key = import.meta.env.VITE_API_KEY
+    axios
+      .get(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${filtered[0].capital}&aqi=no`)
+      .then(res => setWeather({temperature: res.data.current.temp_c, wind: res.data.current.wind_kph}))
+    
     return (
       <>
       <div>
@@ -38,6 +44,11 @@ const App = () => {
             {lang}
           </ul>
           <img src={filtered[0].flags.png} />
+          <h3>weather in {filtered[0].capital}</h3>
+          <ul>
+            <li>temperature: {weather.temperature} C</li>
+            <li>wind: {weather.wind} K/H</li>
+          </ul>
         </div>
       </div>
       </>
